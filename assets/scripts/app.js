6a826f7cf83b57ce9758b64e581277572f810b59
viohlet <violetavenegas@hotmail.com>
2016-10-04 13:10:59 -0400

    Add heroku link to app.js
    Add heroku link to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@
 
 //declare variable app with key api that has a value of our server location
 const app = {
-  host: 'http://localhost:3000',
+  host: 'https://fathomless-ridge-48028.herokuapp.com',
   // user: {
     //id: ?
     //email: ?
