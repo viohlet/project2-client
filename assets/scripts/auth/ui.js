77f5b43ed7079bb8713c96cccd2bacf7ffbf3642
viohlet <violetavenegas@hotmail.com>
2017-02-13 02:35:38 -0500

    Hide and Show Navbar buttons -UI for auth is ready. -User sees register and log-in btns as welcome -User log-ins and sees the rest of the tools -Tools are home, my projects, change pass, log-out

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,17 +3,6 @@
 const app = require('../app');
 
 
-// const showOtherButtons = function (){
-//   // $('.cambiopass').css('display', 'block');
-//   $('.byescreen').css('display', 'block');
-// };
-//
-// const hideLogInsButton = function () {
-//   $('.hiscreen').css('display', 'none');
-//   // $('#loginbutton').css('display', 'none');
-//   // $('#signupbutton').css('display', 'none');
-// };
-
 const hideWelcomeMessage = function () {
   $('#welcome-message').css('display', 'none');
 };
@@ -37,29 +26,24 @@ const signInSuccess = (data) => {
   app.user = data.user;
   document.getElementById("message").innerHTML = 'Log-in successful';
   $('#message').fadeIn('fast').delay(3000).fadeOut('fast');
-  // showOtherButtons();
-  // hideLogInsButton();
+  $('.enter-required-auth').css('display', 'none');
+  $('.homebutton').css('display', 'block');
+  $('.myprojectsbutton').css('display', 'block');
+  $('.change-pass-auth').css('display', 'block');
+  $('#sign-out').css('display', 'block');
   showWorkingArea();
   hideWelcomeMessage();
 };
-
-// const showLogInButtons = function (){
-//   $('.hiscreen').css('display', 'block');
-//   // $('#loginbutton').css('display', 'block');
-//   // $('#signupbutton').css('display', 'block');
-// };
-//
-// const hideOtherButtons = function (){
-//   $('.byescreen').css('display', 'none');
-// };
-
 
 const signOutSuccess = () =>{
   delete app.user;
   // console.log(app);
   $('.app').css("display", "none");
-  // showLogInButtons();
-  // hideOtherButtons();
+  $('.enter-required-auth').css('display', 'block');
+  $('.homebutton').css('display', 'none');
+  $('.myprojectsbutton').css('display', 'none');
+  $('.change-pass-auth').css('display', 'none');
+  $('#sign-out').css('display', 'none');
   hideWorkingArea();
 };
 
